80b8ff3c7802afe029de0c2dda0780d97a73d95f
mrdsantosh <mrdsantosh@yahoo.com>
2020-09-18 15:12:41 +0530

    first commit

---
+++ src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+}
